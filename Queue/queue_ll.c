7cea5d616d7b97bc508e25d1ca7784e431482859
Zach <zadrake@yahoo.com>
2023-08-16 23:05:57 -0700

    pushed queue with ll

---
+++ Queue/queue_ll.c
@@ -0,0 +1,75 @@
+#include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define QUEUE_EMPTY INT_MIN
+
+typedef struct node {
+    int value;
+    struct node* next;
+}node;
+
+typedef struct queue {
+    node *head;
+    node *tail;
+}queue;
+
+void init_queue (queue *q) {
+    q->head = NULL;
+    q->tail = NULL;
+}
+
+bool enqueue(queue *q, int value) {
+    //set up new node
+    node *newnode = malloc(sizeof(node));
+    if (newnode == NULL) return false;
+    newnode->value = value;
+    newnode->next = NULL;
+
+    //check if we have a tail, if we do not add newnode as tail
+    if (q->tail != NULL) {
+        q->tail->next = newnode;
+    }
+    q->tail = newnode;
+
+    //if no head, set newnode to head
+    if(q->head == NULL) {
+        q->head = newnode;
+    }
+    return true;
+}
+
+int dequeue (queue *q) {
+    if (q->head == NULL) return QUEUE_EMPTY;
+    node *tmp = q->head;
+    int result = tmp->value;
+    q->head = q->head->next;
+    if (q->head == NULL) {
+        q->tail = NULL;
+    }
+    free(tmp);
+    return result;
+}
+
+int main() {
+    queue q1, q2, q3;
+
+    init_queue(&q1);
+    init_queue(&q2);
+    init_queue(&q3);
+
+    enqueue(&q1, 10);
+    enqueue(&q1, 20);
+    enqueue(&q2, 100);
+    enqueue(&q2, 200);
+    enqueue(&q3, 1000);
+    enqueue(&q3, 2000);
+
+    int t;
+    while ((t = dequeue(&q2)) != QUEUE_EMPTY) {
+        printf("t = %d\n", t);
+    } 
+
+    return 0;
+}
